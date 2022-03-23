a765e6c2af76b66a6ce14519373fbb1cc069dbe9
Amir <iskandar.amir@gmail.com>
2022-03-23 23:47:45 +0800

    New search design

--- components/search.js
+++ components/search.js
@@ -1,5 +1,20 @@
 import { motion } from "framer-motion";
 import { useRef } from "react";
+
+import {
+  ArrowLeftIcon,
+  ArrowRightIcon,
+  DotsCircleHorizontalIcon,
+  LockClosedIcon,
+} from "@heroicons/react/outline";
+import {
+  CheckIcon,
+  DotsHorizontalIcon,
+  RefreshIcon,
+  SearchCircleIcon,
+  CheckCircleIcon,
+  StarIcon,
+} from "@heroicons/react/solid";
 export default function Search(props) {
   const { setPhase, phase, setData } = props;
   const queryRef = useRef();
@@ -20,33 +35,72 @@ export default function Search(props) {
 
   return (
     <motion.div
-      className={`m-auto flex w-full max-w-xl ${phase === "show" && "mt-20"}`}
+      className={`m-auto flex flex-col w-full max-w-xl ${
+        phase === "show" && "mt-20"
+      }`}
       layout
     >
-      <div className="m-auto flex w-full max-w-xl mx-1">
-        <span className="bg-gray-500 rounded-l-xl p-1">dateof.my/</span>
+      <div className="m-auto flex w-full mx-1 bg-gray-300 h-10 rounded-t-xl shadow-xl">
+        <div>
+          <DotsHorizontalIcon className="w-12 h-full" />
+        </div>
+        <div className="bg-gray-100 mt-2 px-2 rounded-t-xl w-40 flex">
+          <div className="my-auto flex">
+            <StarIcon className="w-4 my-auto" />
+            Date of
+          </div>
+        </div>
+      </div>
+      <div className="m-auto flex w-full mx-1 bg-gray-100 p-1">
+        <div>
+          <ArrowLeftIcon className="w-5 h-full" />
+        </div>
+        <div>
+          <ArrowRightIcon className="w-5 h-full" />
+        </div>
+        <div>
+          <RefreshIcon className="w-5 h-full" />
+        </div>
+        <div className="rounded-l-xl p-1 bg-white flex">
+          <div>
+            <LockClosedIcon className="w-5 h-full" />
+          </div>
+          <div>dateof.my/</div>
+        </div>
         <input
           ref={queryRef}
           type="text"
-          placeholder="Last Name"
-          className="text-sm py-1 px-2 focus:ring-2 focus:ring-gray-300 block border border-gray-300 w-full"
+          placeholder=""
+          className="text-sm py-1 px-2 ring-0 focus:ring-0 block w-full"
           onChange={() => {
             setPhase("start");
           }}
         ></input>
         <motion.button
+          layout
           whileHover={{ scale: 1.1 }}
           whileTap={{ scale: 0.9 }}
-          className="bg-green-500 rounded-r-xl p-1"
+          className="bg-white rounded-r-xl p-1"
           onClick={() => {
             GetQuote();
           }}
         >
-          {phase === "start"
-            ? "Check"
-            : phase === "loading"
-            ? "Loading"
-            : "Done!"}
+          {phase === "start" ? (
+            <div className="flex bg-green-500 rounded-xl px-2">
+              <div className="my-auto">Search</div>
+              <SearchCircleIcon className="w-5 h-full my-auto" />
+            </div>
+          ) : phase === "loading" ? (
+            <div className="flex bg-gray-500 rounded-xl px-2">
+              <div className="my-auto">Loading...</div>
+              <DotsCircleHorizontalIcon className="w-5 h-full animate animate-spin my-auto" />
+            </div>
+          ) : (
+            <div className="flex bg-red-500 rounded-xl px-2">
+              <div className="my-auto">Book!</div>
+              <CheckCircleIcon className="w-5 h-full my-auto" />
+            </div>
+          )}
         </motion.button>
       </div>
     </motion.div>
--- pages/api/quote.js
+++ pages/api/quote.js
@@ -1,8 +1,12 @@
+import { sleep } from "../../lib/site";
+
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
   const { q: query } = req.query;
-
+  console.log("start");
+  await sleep(2000);
+  console.log("end");
   res.status(200).json({
     query: query,
     start: "Mac 2022",
